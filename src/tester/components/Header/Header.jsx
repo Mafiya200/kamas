58fd643980c746ae9c9c9c1315ab2d2ea28a346e
Venom <dracon.24041230@mail.ru>
2023-10-04 15:22:53 +0300

    0.1

---
+++ src/tester/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+
+import styleHeader from "./Header.module.css";
+
+
+const Header = function () {
+
+
+  return (
+    <header className={styleHeader.header}>
+      <a className={styleHeader.logo__link} href="https://yandex.ru/search/?text=%D0%BB%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF+%D1%81+%D0%BF%D1%80%D0%BE%D0%B7%D1%80%D0%B0%D1%87%D0%BD%D1%8B%D0%BC+%D1%84%D0%BE%D0%BD%D0%BE%D0%BC&lr=35">
+        <img src='https://www.pinclipart.com/picdir/big/559-5592685_oxford-brookes-university-smiths-station-high-school-black.png' alt="123" /></a>
+      <div className={styleHeader.header__container}></div>
+    </header>
+  );
+
+}
+
+export default Header;
